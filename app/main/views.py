f86ffaf5ab189f48074a50ec629a122b7cb592f0
RonaldKiprotich <protich12@gmail.com>
2020-10-24 21:02:18 +0300

    fix bugs

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask_login import login_required
 from flask import  render_template
-from .import main
+from . import main
 
 @main.route('/')
 def index():
--- app/models.py
+++ app/models.py
@@ -18,11 +18,11 @@ class User(UserMixin,db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_secure = generate_password_hash(password)
 
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_secure,password)
 
     
     @login_manager.user_loader
