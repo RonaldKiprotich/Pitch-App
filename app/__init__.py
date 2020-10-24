daf08d11e3e64797dc7fcb713c794995c1569c1b
RonaldKiprotich <protich12@gmail.com>
2020-10-24 20:11:54 +0300

    add login page

--- app/__init__.py
+++ app/__init__.py
@@ -28,8 +28,18 @@ def create_app(config_name):
 
 
 
+
+
+#Registering the blueprint
+
+
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
 
 
     return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,1 +1,7 @@
 from flask_login import login_required
+from flask import  render_template
+from .import main
+
+@main.route('/')
+def index():
+    return render_template('index.html')
